7fd539da9fc93b2a0d80513e50898f7b4b52f291
Daniel Killenberger <daniel.killenberger@gmail.com>
2018-09-06 16:56:26 +0200

    Add some more helper functions and some first tests. Implement non working calculateIntersection. Rename getOtherCornerPoints -> getNonOriginCornerPoints

--- src/main/java/com/unitelabsrectanglechallenge/Rectangle.java
+++ src/main/java/com/unitelabsrectanglechallenge/Rectangle.java
@@ -1,5 +1,7 @@
 package com.unitelabsrectanglechallenge;
 
+import java.util.LinkedList;
+
 class Rectangle {
     final Vector2D origin;
     final int width, height;
@@ -11,9 +13,28 @@ class Rectangle {
     }
 
     /**
-     * @return Array of 3 other corner points in counterclockwise order.
+     * @param rectangle1
+     * @param rectangle2
+     * @return RectangleFromIntersection with both intersecting rectangles as parents
      */
-    Vector2D[] getOtherCornerPoints() {
+    static RectangleFromIntersection calculateIntersection(Rectangle rectangle1, Rectangle rectangle2) {
+        LinkedList<Vector2D> cornerPointsIntersectingThis = rectangle2.getIntersectingCornerPoints(rectangle1);
+        if (cornerPointsIntersectingThis.size() == 4) {
+            return new RectangleFromIntersection(rectangle1.origin, rectangle1.width, rectangle1.height, rectangle2, rectangle1);
+        }
+
+        LinkedList<Vector2D> cornerPointsIntersectingOther = rectangle1.getIntersectingCornerPoints(rectangle2);
+        if (cornerPointsIntersectingOther.size() == 4) {
+            return new RectangleFromIntersection(rectangle2.origin, rectangle2.width, rectangle2.height, rectangle2, rectangle1);
+        }
+        return null;
+    }
+
+
+    /**
+     * @return Array of 3 non-origin corner points in counterclockwise order.
+     */
+    Vector2D[] getNonOriginCornerPoints() {
         return new Vector2D[]{
                 origin.plus(new Vector2D(0, height)),
                 origin.plus(new Vector2D(0, height)).plus(new Vector2D(width, 0)),
@@ -21,21 +42,51 @@ class Rectangle {
         };
     }
 
-    boolean isPointInRectangle(Vector2D point) {
-        Vector2D[] cornerPoints = getOtherCornerPoints();
-
-        final Line leftLine = new Line(origin, cornerPoints[0]);
-        final Line topLine = new Line(origin, cornerPoints[2]);
-        final Line bottomLine = new Line(cornerPoints[2], cornerPoints[1]);
-        final Line rightLine = new Line(cornerPoints[0], cornerPoints[1]);
-
-        return (topLine.isPointOnLine(point) == 1 &&
-                leftLine.isPointOnLine(point) == 1 &&
-                bottomLine.isPointOnLine(point) == -1 &&
-                rightLine.isPointOnLine(point) == -1);
+    /**
+     * @return Array of all corner points in counterclockwise order starting from origin.
+     */
+    Vector2D[] getCornerPoints() {
+        Vector2D[] otherCornerPoints = getNonOriginCornerPoints();
+        return new Vector2D[]{origin, otherCornerPoints[0], otherCornerPoints[1], otherCornerPoints[2]};
     }
 
-    RectangleFromIntersection calculateIntersection(Rectangle rectangle) {
-        return new RectangleFromIntersection(origin, width, height, this, this);
+    /**
+     * @return Array of all lines defining the rectangle in counterclockwise order starting from the left.
+     */
+    Line[] getLines() {
+        Vector2D[] cornerPoints = getNonOriginCornerPoints();
+
+        final Line leftLine = new Line(origin, cornerPoints[0]);
+        final Line bottomLine = new Line(cornerPoints[0], cornerPoints[1]);
+        final Line rightLine = new Line(cornerPoints[1], cornerPoints[2]);
+        final Line topLine = new Line(origin, cornerPoints[2]);
+
+        return new Line[]{leftLine, bottomLine, rightLine, topLine};
+    }
+
+    boolean isPointInRectangle(Vector2D point) {
+        Line[] lines = getLines();
+
+        return (lines[0].whereIsPointRelativeToLine(point) == 1 &&
+                lines[1].whereIsPointRelativeToLine(point) == -1 &&
+                lines[2].whereIsPointRelativeToLine(point) == -1 &&
+                lines[3].whereIsPointRelativeToLine(point) == 1);
+    }
+
+
+    /**
+     *
+     * @param rectangle
+     * @return LinkedList<Vector2D> with points of the other rectangle within this rectangle
+     */
+    LinkedList<Vector2D> getIntersectingCornerPoints(Rectangle rectangle) {
+        Vector2D[] cornerPoints = rectangle.getCornerPoints();
+        LinkedList<Vector2D> cornerPointsIntersecting = new LinkedList<>();
+        for (var cornerPoint : cornerPoints) {
+            if (isPointInRectangle(cornerPoint)) {
+                cornerPointsIntersecting.add(cornerPoint);
+            }
+        }
+        return cornerPointsIntersecting;
     }
 }
--- src/test/java/com/unitelabsrectanglechallenge/RectangleTest.java
+++ src/test/java/com/unitelabsrectanglechallenge/RectangleTest.java
@@ -2,6 +2,8 @@ package com.unitelabsrectanglechallenge;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleTest {
@@ -9,10 +11,41 @@ class RectangleTest {
     void GetOtherCornerPoints() {
         Rectangle rec = new Rectangle(new Vector2D(0, 0), 3, 3);
         //Check if in counterclockwise order
-        Vector2D[] otherCornerPoints = rec.getOtherCornerPoints();
+        Vector2D[] otherCornerPoints = rec.getNonOriginCornerPoints();
         assertEquals(otherCornerPoints[0], new Vector2D(0, 3));
         assertEquals(otherCornerPoints[1], new Vector2D(3, 3));
         assertEquals(otherCornerPoints[2], new Vector2D(3, 0));
+    }
+
+    @Test
+    void GetCornerPoints() {
+        Rectangle rec = new Rectangle(new Vector2D(0, 0), 3, 3);
+
+        Vector2D[] cornerPoints = rec.getCornerPoints();
+        assertEquals(cornerPoints[0], new Vector2D(0, 0));
+        assertEquals(cornerPoints[1], new Vector2D(0, 3));
+        assertEquals(cornerPoints[2], new Vector2D(3, 3));
+        assertEquals(cornerPoints[3], new Vector2D(3, 0));
+    }
+
+    @Test
+    void GetIntersectingCornerPoints() {
+        Rectangle rec1 = new Rectangle(new Vector2D(0, 0), 3, 3);
+        Rectangle rec2 = new Rectangle(new Vector2D(0, 0), 2, 2);
+
+        assertEquals(1, rec1.getIntersectingCornerPoints(rec2).size());
+        assertEquals(new Vector2D(2,2), rec1.getIntersectingCornerPoints(rec2).get(0));
+    }
+
+    @Test
+    void GetLines() {
+        Rectangle rec = new Rectangle(new Vector2D(0, 0), 3, 3);
+
+        Line[] lines = rec.getLines();
+        assertEquals(lines[0], new Line(new Vector2D(0, 0), new Vector2D(0,3)));
+        assertEquals(lines[1], new Line(new Vector2D(0, 3), new Vector2D(3,3)));
+        assertEquals(lines[2], new Line(new Vector2D(3, 3), new Vector2D(3,0)));
+        assertEquals(lines[3], new Line(new Vector2D(3, 0), new Vector2D(0,0)));
     }
 
     @Test
