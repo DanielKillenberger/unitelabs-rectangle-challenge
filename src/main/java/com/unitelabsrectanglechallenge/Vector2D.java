10996fff56c93522d65122096b315e15dd1f0ac2
Daniel Killenberger <daniel.killenberger@gmail.com>
2018-09-06 15:11:32 +0200

    Add equals and hashCode overrides to Vector2d

--- src/main/java/com/unitelabsrectanglechallenge/Vector2D.java
+++ src/main/java/com/unitelabsrectanglechallenge/Vector2D.java
@@ -1,5 +1,7 @@
 package com.unitelabsrectanglechallenge;
 
+import java.util.Objects;
+
 class Vector2D {
     final int X, Y;
 
@@ -14,5 +16,22 @@ class Vector2D {
 
     Vector2D minus(Vector2D b) {
         return new Vector2D(Math.subtractExact(X, b.X), Math.subtractExact(Y, b.Y));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this){
+            return true;
+        }
+        if (!(other instanceof Vector2D)) {
+            return false;
+        }
+        Vector2D o = (Vector2D) other;
+        return o.X == X && o.Y == Y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(X, Y);
     }
 }
